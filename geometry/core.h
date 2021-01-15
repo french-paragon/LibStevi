9fb2a7bc78aa8d9733a211e712a409e4bbcf8501
french-paragon <french.paragon@gmail.com>
2021-01-15 13:54:44 +0800

    Add license information to the files.

--- geometry/core.h
+++ geometry/core.h
@@ -12,6 +12,24 @@ Eigen::Vector3f unskew(Eigen::Matrix3f const& m);
 Eigen::Matrix3d skewD(Eigen::Vector3d const& v);
 Eigen::Vector3d unskewD(Eigen::Matrix3d const& m);
 
+/*LibStevi, or the Stereo Vision Library, is a collection of utilities for 3D computer vision.
+
+Copyright (C) 2021  Paragon<french.paragon@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 enum class Axis : char {
 	X,
 	Y,
