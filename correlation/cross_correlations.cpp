35e121298ac1d81f793e0eadff6382f89f3491e9
french-paragon <french.paragon@gmail.com>
2021-08-05 10:13:25 +0800

    Add missing names to cost functions traits classes.

--- correlation/cross_correlations.cpp
+++ correlation/cross_correlations.cpp
@@ -3,9 +3,14 @@
 namespace StereoVision {
 namespace Correlation {
 
+const std::string MatchingFunctionTraits<matchingFunctions::CC>::Name = "CC";
 const std::string MatchingFunctionTraits<matchingFunctions::NCC>::Name = "NCC";
 const std::string MatchingFunctionTraits<matchingFunctions::SSD>::Name = "SSD";
 const std::string MatchingFunctionTraits<matchingFunctions::SAD>::Name = "SAD";
+const std::string MatchingFunctionTraits<matchingFunctions::ZCC>::Name = "ZCC";
+const std::string MatchingFunctionTraits<matchingFunctions::ZNCC>::Name = "ZNCC";
+const std::string MatchingFunctionTraits<matchingFunctions::ZSSD>::Name = "ZSSD";
+const std::string MatchingFunctionTraits<matchingFunctions::ZSAD>::Name = "ZSAD";
 
 } //namespace Correlation
 } //namespace StereoVision
