2cd6210b45678e3dec535887d17101632e771603
french-paragon <french.paragon@gmail.com>
2022-02-28 11:20:39 +0800

    Add an helper function to convert euler xyz angles to rotation matrices.

--- geometry/rotations.h
+++ geometry/rotations.h
@@ -60,6 +60,24 @@ Eigen::Vector3d inverseRodriguezFormulaD(Eigen::Matrix3d const& R);
 Eigen::Matrix3d diffRodriguezLieAlgebraD(Eigen::Vector3d const& r);
 Eigen::Matrix3d diffRodriguezD(Eigen::Vector3d const& r, Axis direction);
 
+template <typename Scalar>
+Eigen::Matrix<Scalar, 3, 3> eulerRadXYZToRotation(Scalar eulerX,
+												  Scalar eulerY,
+												  Scalar eulerZ) {
+
+	return (Eigen::AngleAxis<Scalar>(eulerX, Eigen::Matrix<Scalar,3,1>::UnitX())*
+			Eigen::AngleAxis<Scalar>(eulerY, Eigen::Matrix<Scalar,3,1>::UnitY())*
+			Eigen::AngleAxis<Scalar>(eulerZ, Eigen::Matrix<Scalar,3,1>::UnitZ())).toRotationMatrix();
+
+}
+
+template <typename Scalar>
+Eigen::Matrix<Scalar, 3, 3> eulerDegXYZToRotation(Scalar eulerX,
+												  Scalar eulerY,
+												  Scalar eulerZ) {
+	return eulerRadXYZToRotation(eulerX/180*M_PI, eulerY/180*M_PI, eulerZ/180*M_PI);
+}
+
 } // namespace Geometry
 } //namespace StereoVision
 
