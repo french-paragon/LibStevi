b1b606e093ff0bc29fb6c9263406ff6e724134f1
french-paragon <french.paragon@gmail.com>
2022-03-07 15:12:12 +0800

    Extend the image interpolation function so that single channels images are accepted as well.

--- interpolation/interpolation.cpp
+++ interpolation/interpolation.cpp
@@ -24,24 +24,50 @@ namespace Interpolation {
 Multidim::Array<float, 3> interpolateImage(Multidim::Array<float, 3> const& imInput,
 										   Multidim::Array<float, 3> const& coordinates) {
 
-	Multidim::Array<float, 3> imgOut(coordinates.shape()[0], coordinates.shape()[1], 3);
+	auto s = imInput.shape();
 
-	for (int i = 0; i < 3; i++) {
+	if (s[2] == 3) {
+
+		Multidim::Array<float, 3> imgOut(coordinates.shape()[0], coordinates.shape()[1], 3);
+
+		for (int i = 0; i < 3; i++) {
+			Multidim::Array<float, 3>* nonConst = const_cast<Multidim::Array<float, 3>*>(&imInput);
+
+			Multidim::Array<float, 2> inChannel = nonConst->subView(Multidim::DimSlice(),
+																	Multidim::DimSlice(),
+																	Multidim::DimIndex(i));
+
+			Multidim::Array<float, 2> outChannel = imgOut.subView(Multidim::DimSlice(),
+																  Multidim::DimSlice(),
+																  Multidim::DimIndex(i));
+
+			outChannel.copyData(interpolate<2, 2, float, pyramidFunction<float, 2>, 0>(inChannel, coordinates));
+
+		}
+
+		return imgOut;
+
+	} else if (s[2] == 1) {
+
+		Multidim::Array<float, 3> imgOut(coordinates.shape()[0], coordinates.shape()[1], 1);
+
 		Multidim::Array<float, 3>* nonConst = const_cast<Multidim::Array<float, 3>*>(&imInput);
 
 		Multidim::Array<float, 2> inChannel = nonConst->subView(Multidim::DimSlice(),
 																Multidim::DimSlice(),
-																Multidim::DimIndex(i));
+																Multidim::DimIndex(0));
 
 		Multidim::Array<float, 2> outChannel = imgOut.subView(Multidim::DimSlice(),
 															  Multidim::DimSlice(),
-															  Multidim::DimIndex(i));
+															  Multidim::DimIndex(0));
 
 		outChannel.copyData(interpolate<2, 2, float, pyramidFunction<float, 2>, 0>(inChannel, coordinates));
 
+
+		return imgOut;
 	}
 
-	return imgOut;
+	return Multidim::Array<float, 3>();
 
 }
 
