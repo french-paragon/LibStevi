8b7beac5e04161e81979399ba8bb3202e9ac89ae
french-paragon <french.paragon@gmail.com>
2023-05-01 17:14:56 +0200

    Add an image adapter for grayscale arrays

--- examples/stevimg_viewer/main.cpp
+++ examples/stevimg_viewer/main.cpp
@@ -30,8 +30,10 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #include <QMap>
 #include <QVector>
 
-template<typename T>
+template<typename T, int nDim>
 int displayImage(std::string const& imageName, QMap<QString, QString> const& options, QApplication & application, QTextStream & outStream) {
+
+    static_assert (nDim == 3 or nDim == 2, "Can only process colored of grayscale images");
 
     T blackLevel = StereoVision::TypesManipulations::defaultBlackLevel<T>();
     T whiteLevel = StereoVision::TypesManipulations::defaultWhiteLevel<T>();
@@ -66,7 +68,7 @@ int displayImage(std::string const& imageName, QMap<QString, QString> const& opt
         }
     }
 
-    Multidim::Array<T, 3> img = StereoVision::IO::readStevimg<T, 3>(imageName);
+    Multidim::Array<T, nDim> img = StereoVision::IO::readStevimg<T, nDim>(imageName);
 
     if (img.empty()) {
         outStream << "impossible to read image: " << QString(imageName.c_str()) << Qt::endl;
@@ -76,15 +78,30 @@ int displayImage(std::string const& imageName, QMap<QString, QString> const& opt
         outStream << "Image shape: " << img.shape()[0] << "x" << img.shape()[1] << "x" <<  img.shape()[2] << Qt::endl;
     }
 
-    StereoVision::Gui::ArrayDisplayAdapter<T> imgAdapter(&img, blackLevel, whiteLevel);
+    Multidim::Array<T, 3>* coloredImg = nullptr;
+    Multidim::Array<T, 2>* grayImg = nullptr;
+
+    if (nDim == 3) {
+        coloredImg = reinterpret_cast<Multidim::Array<T, 3>*>(&img);
+    } else if (nDim == 2) {
+        grayImg = reinterpret_cast<Multidim::Array<T, 2>*>(&img);
+    }
+
+    StereoVision::Gui::ArrayDisplayAdapter<T> cimgAdapter(coloredImg, blackLevel, whiteLevel);
+    StereoVision::Gui::GrayscaleArrayDisplayAdapter<T> gimgAdapter(grayImg, blackLevel, whiteLevel);
 
     if (options.contains("--channels")) {
         QVector<QString> channelsNames = options.value("--channels").split(",").toVector();
-        imgAdapter.configureOriginalChannelDisplay(channelsNames);
+        cimgAdapter.configureOriginalChannelDisplay(channelsNames);
     }
 
     QImageDisplay::ImageWindow imgWindow;
-    imgWindow.setImage(&imgAdapter);
+
+    if (nDim == 3) {
+        imgWindow.setImage(&cimgAdapter);
+    } else if (nDim == 2) {
+        imgWindow.setImage(&gimgAdapter);
+    }
 
     if (options.contains("--title")) {
         imgWindow.setWindowTitle(options.value("--title"));
@@ -130,57 +147,79 @@ int main(int argc, char** argv) {
     std::string inFileName = arguments[0].toStdString();
 
     //8 bit integer
+    if (StereoVision::IO::stevImgFileMatchTypeAndDim<int8_t, 2>(inFileName)) {
+        return displayImage<int8_t,2>(inFileName, options, app, out);
+    }
     if (StereoVision::IO::stevImgFileMatchTypeAndDim<int8_t, 3>(inFileName)) {
-        return displayImage<int8_t>(inFileName, options, app, out);
+        return displayImage<int8_t,3>(inFileName, options, app, out);
     }
 
+    if (StereoVision::IO::stevImgFileMatchTypeAndDim<uint8_t, 2>(inFileName)) {
+        return displayImage<uint8_t,2>(inFileName, options, app, out);
+    }
     if (StereoVision::IO::stevImgFileMatchTypeAndDim<uint8_t, 3>(inFileName)) {
-        return displayImage<uint8_t>(inFileName, options, app, out);
+        return displayImage<uint8_t,3>(inFileName, options, app, out);
     }
 
     //16 bit integer
+    if (StereoVision::IO::stevImgFileMatchTypeAndDim<int16_t, 2>(inFileName)) {
+        return displayImage<int16_t,2>(inFileName, options, app, out);
+    }
     if (StereoVision::IO::stevImgFileMatchTypeAndDim<int16_t, 3>(inFileName)) {
-        return displayImage<int16_t>(inFileName, options, app, out);
+        return displayImage<int16_t,3>(inFileName, options, app, out);
     }
 
+    if (StereoVision::IO::stevImgFileMatchTypeAndDim<uint16_t, 2>(inFileName)) {
+        return displayImage<uint16_t,2>(inFileName, options, app, out);
+    }
     if (StereoVision::IO::stevImgFileMatchTypeAndDim<uint16_t, 3>(inFileName)) {
-        return displayImage<uint16_t>(inFileName, options, app, out);
+        return displayImage<uint16_t,3>(inFileName, options, app, out);
     }
 
     //32 bit integer
+    if (StereoVision::IO::stevImgFileMatchTypeAndDim<int32_t, 2>(inFileName)) {
+        return displayImage<int32_t,2>(inFileName, options, app, out);
+    }
     if (StereoVision::IO::stevImgFileMatchTypeAndDim<int32_t, 3>(inFileName)) {
-        return displayImage<int32_t>(inFileName, options, app, out);
+        return displayImage<int32_t,3>(inFileName, options, app, out);
     }
 
+    if (StereoVision::IO::stevImgFileMatchTypeAndDim<uint32_t, 2>(inFileName)) {
+        return displayImage<uint32_t,2>(inFileName, options, app, out);
+    }
     if (StereoVision::IO::stevImgFileMatchTypeAndDim<uint32_t, 3>(inFileName)) {
-        return displayImage<uint32_t>(inFileName, options, app, out);
+        return displayImage<uint32_t,3>(inFileName, options, app, out);
     }
 
     //64 bit integer
+    if (StereoVision::IO::stevImgFileMatchTypeAndDim<int64_t, 2>(inFileName)) {
+        return displayImage<int64_t,2>(inFileName, options, app, out);
+    }
     if (StereoVision::IO::stevImgFileMatchTypeAndDim<int64_t, 3>(inFileName)) {
-        return displayImage<int64_t>(inFileName, options, app, out);
+        return displayImage<int64_t,3>(inFileName, options, app, out);
     }
 
+    if (StereoVision::IO::stevImgFileMatchTypeAndDim<uint64_t, 2>(inFileName)) {
+        return displayImage<uint64_t,2>(inFileName, options, app, out);
+    }
     if (StereoVision::IO::stevImgFileMatchTypeAndDim<uint64_t, 3>(inFileName)) {
-        return displayImage<uint64_t>(inFileName, options, app, out);
+        return displayImage<uint64_t,3>(inFileName, options, app, out);
     }
 
     //floating points
-    if (StereoVision::IO::stevImgFileMatchTypeAndDim<float, 3>(inFileName)) {
-        return displayImage<float>(inFileName, options, app, out);
+    if (StereoVision::IO::stevImgFileMatchTypeAndDim<float, 2>(inFileName)) {
+        return displayImage<float,2>(inFileName, options, app, out);
     }
-
     if (StereoVision::IO::stevImgFileMatchTypeAndDim<float, 3>(inFileName)) {
-        return displayImage<float>(inFileName, options, app, out);
+        return displayImage<float,3>(inFileName, options, app, out);
     }
 
     //double precision floating points
-    if (StereoVision::IO::stevImgFileMatchTypeAndDim<double, 3>(inFileName)) {
-        return displayImage<double>(inFileName, options, app, out);
+    if (StereoVision::IO::stevImgFileMatchTypeAndDim<double, 2>(inFileName)) {
+        return displayImage<double,2>(inFileName, options, app, out);
     }
-
     if (StereoVision::IO::stevImgFileMatchTypeAndDim<double, 3>(inFileName)) {
-        return displayImage<double>(inFileName, options, app, out);
+        return displayImage<double,3>(inFileName, options, app, out);
     }
 
     out << "Input image does not match expected type or shape for a displayable image!" << Qt::endl;
--- gui/arraydisplayadapter.h
+++ gui/arraydisplayadapter.h
@@ -29,6 +29,8 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #include <MultidimArrays/MultidimArrays.h>
 
+#include <optional>
+
 #include "utils/types_manipulations.h"
 
 namespace StereoVision {
@@ -167,6 +169,154 @@ protected:
     bool _displayOriginalChannels;
     QVector<QString> _channelsName;
 
+};
+
+
+template<typename Array_T, Multidim::ArrayDataAccessConstness viewConstness = Multidim::NonConstView>
+class GrayscaleArrayDisplayAdapter : public QImageDisplay::ImageAdapter
+{
+public:
+
+    GrayscaleArrayDisplayAdapter(Multidim::Array<Array_T, 2, viewConstness> const* array,
+                        Array_T blackLevel = TypesManipulations::defaultBlackLevel<Array_T>(),
+                        Array_T whiteLevel = TypesManipulations::defaultWhiteLevel<Array_T>(),
+                        int xAxis = 1,
+                        int yAxis = 0,
+                        QObject* parent = nullptr) :
+        QImageDisplay::ImageAdapter(parent),
+        _array(array),
+        _x_axis(xAxis),
+        _y_axis(yAxis),
+        _black_level(blackLevel),
+        _white_level(whiteLevel),
+        _colorMap(std::nullopt)
+    {
+
+    }
+
+    QSize getImageSize() const override {
+        if (_array == nullptr) {
+            return QSize();
+        }
+        return QSize(_array->shape()[_x_axis], _array->shape()[_y_axis]);
+    }
+
+    QColor getColorAtPoint(int x, int y) const override{
+
+        if (_array == nullptr) {
+            return QColor();
+        }
+
+        std::array<int, 2> idx;
+        idx[_x_axis] = x;
+        idx[_y_axis] = y;
+
+        if (_colorMap.has_value()) {
+            return valueToColorWithColorMap(_array->valueOrAlt(idx, 0));
+        }
+
+        QColor ret;
+
+        ret.setRed(valueToColor(_array->valueOrAlt(idx, 0)));
+        ret.setGreen(valueToColor(_array->valueOrAlt(idx, 0)));
+        ret.setBlue(valueToColor(_array->valueOrAlt(idx, 0)));
+
+        return ret;
+    }
+
+    QVector<ChannelInfo> getOriginalChannelsInfos(QPoint const& pos) const override {
+
+        if (!_displayOriginalChannel) {
+            return QImageDisplay::ImageAdapter::getOriginalChannelsInfos(pos);
+        }
+
+        QVector<ChannelInfo> ret(1);
+
+        QString formatStr = "%1 ";
+
+        std::array<int, 2> idx;
+        idx[_x_axis] = pos.x();
+        idx[_y_axis] = pos.y();
+
+        ret[0].channelName = _channelName;
+
+        double tmp = _array->valueOrAlt(idx, 0);
+
+        if (std::is_floating_point_v<Array_T>) {
+            ret[0].channelValue = QString(formatStr).arg(tmp, 0, 'g', 3);
+        } else {
+            ret[0].channelValue = QString(formatStr).arg(_array->valueOrAlt(idx, 0));
+        }
+
+        return ret;
+
+    }
+
+    inline void configureOriginalChannelDisplay( QString const& channel) {
+        _displayOriginalChannel = true;
+        _channelName = channel;
+    }
+
+    inline void clearOriginalChannelDisplay() {
+        _displayOriginalChannel = false;
+        _channelName.clear();
+    }
+
+    inline void setColorMap(std::function<QColor(float)> const& colorMap) {
+        _colorMap = colorMap;
+    }
+
+    inline void clearColorMap(std::function<QColor(float)> const& colorMap) {
+        _colorMap = std::nullopt;
+    }
+
+protected:
+
+    using ComputeType = TypesManipulations::accumulation_extended_t<Array_T>;
+
+    inline uint8_t valueToColor(Array_T const& value) const {
+
+        if (value < _black_level) {
+            return 0;
+        }
+
+        if (value >= _white_level) {
+            return 255;
+        }
+
+        ComputeType transformed = (255*(static_cast<ComputeType>(value) - static_cast<ComputeType>(_black_level)))
+                /(_white_level - _black_level);
+
+        return static_cast<uint8_t>(transformed);
+    }
+
+    inline QColor valueToColorWithColorMap(Array_T const& value) const {
+
+        const std::function<QColor(float)>& cm = _colorMap.value();
+
+        if (value < _black_level) {
+            return cm(0);
+        }
+
+        if (value >= _white_level) {
+            return cm(1);
+        }
+
+        return cm(static_cast<float>(value - _black_level)/static_cast<float>(_white_level - _black_level));
+    }
+
+    Multidim::Array<Array_T, 2, viewConstness> const* _array;
+
+    int _x_axis;
+    int _y_axis;
+
+    Array_T _black_level;
+    Array_T _white_level;
+
+    bool _displayOriginalChannel;
+    QString _channelName;
+
+    std::optional<std::function<QColor(float)>> _colorMap;
 };
 
 } //namespace Gui
