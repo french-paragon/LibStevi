7960140ce3071441dcdd9f8293320bb52342609b
french-paragon <french.paragon@gmail.com>
2023-08-21 17:15:40 +0200

    Add the FAST corner detector to sparse matching utils

--- sparseMatching/cornerDetectors.h
+++ sparseMatching/cornerDetectors.h
@@ -5,6 +5,7 @@
 
 #include "../imageProcessing/convolutions.h"
 #include "../imageProcessing/standardConvolutionFilters.h"
+#include "../imageProcessing/morphologicalOperators.h"
 
 namespace StereoVision {
 namespace SparseMatching {
@@ -13,7 +14,7 @@ namespace SparseMatching {
  * \brief HarrisCornerMMat compute the coefficient of the M windows of the Harris corner detector
  * \param img the image to perform the computation on
  * \param lowPassRadius the radius of the low pass filter applied to smooth the image.
- * \return
+ * \return a tensor with the 3 components of the M matrix
  */
 template<typename T>
 Multidim::Array<T, 3> HarrisCornerMMat(Multidim::Array<T, 2, Multidim::ConstView> const& img, int lowPassRadius = 3) {
@@ -22,12 +23,12 @@ Multidim::Array<T, 3> HarrisCornerMMat(Multidim::Array<T, 2, Multidim::ConstView
 
     using Padding = ImageProcessing::Convolution::PaddingInfos;
 
-    using FiltType = ImageProcessing::Convolution::Filter<float, MovingAxis, MovingAxis>;
+    using FiltType = ImageProcessing::Convolution::Filter<T, MovingAxis, MovingAxis>;
 
     constexpr bool normalize = false;
     float sigma = static_cast<float>(lowPassRadius+1)/2;
 
-    Padding diff_pad(lowPassRadius, ImageProcessing::Convolution::PaddingType::Mirror);
+    Padding diff_pad(1, ImageProcessing::Convolution::PaddingType::Mirror);
 
     std::array<FiltType, 2> finiteDifferencesFilters =
             ImageProcessing::Convolution::finiteDifferencesKernels<T>(MovingAxis(diff_pad), MovingAxis(diff_pad));
@@ -76,6 +77,97 @@ Multidim::Array<T, 3> HarrisCornerMMat(Multidim::Array<T, 2, Multidim::ConstView
 
             T d0 = diffs[0].valueUnchecked(i,j);
             T d1 = diffs[1].valueUnchecked(i,j);
+
+            M.atUnchecked(i,j,0) = d0*d0;
+            M.atUnchecked(i,j,1) = d0*d1;
+            M.atUnchecked(i,j,2) = d1*d1;
+
+        }
+    }
+
+    return M;
+
+}
+
+/*!
+ * \brief maskedHarrisCornerMMat
+ * \param img the image to run the detection onto.
+ * \param mask the mask indicating which pixels needs to be computed.
+ * \param lowPassRadius the radius of the low pass filter applied to smooth the image.
+ * \return a tensor whit the 3 components of the M matrix at pixels where the mask is true.
+ */
+template<typename T>
+Multidim::Array<T, 3> maskedHarrisCornerMMat(Multidim::Array<T, 2, Multidim::ConstView> const& img,
+                                             Multidim::Array<bool, 2, Multidim::ConstView> const& mask,
+                                             int lowPassRadius = 3) {
+
+    int filtSize = 2*lowPassRadius+1;
+    std::vector<T> gaussianCoeffs(filtSize);
+
+    float sigma = static_cast<float>(lowPassRadius+1)/2;
+    float var = sigma*sigma;
+
+    for (int i = 0; i < filtSize; i++) {
+        int delta = i-lowPassRadius;
+        gaussianCoeffs[i] = std::exp(-(delta*delta)/var);
+    }
+
+    std::array<int,2> imShape = img.shape();
+
+    std::array<int,3> shape = {imShape[0], imShape[1], 3};
+    Multidim::Array<T, 3> M(shape);
+
+    for (int i = 0; i < imShape[0]; i++) {
+        for (int j = 0; j < imShape[1]; j++) {
+
+            bool inMask = mask.valueUnchecked(i,j);
+
+            if (!inMask) {
+                M.atUnchecked(i,j,0) = 0;
+                M.atUnchecked(i,j,1) = 0;
+                M.atUnchecked(i,j,2) = 0;
+                continue;
+            }
+
+            std::array<std::array<T,3>,3> filtered;
+
+            for (int k = 0; k < 3; k++) {
+                for (int l = 0; l < 3; l++) {
+
+                    T accumulated = 0;
+                    int d0 = k-1;
+                    int d1 = l-1;
+
+                    for (int f = 0; f < filtSize; f++) {
+
+                        int df = f-lowPassRadius;
+
+                        for (int g = 0; g < filtSize; g++) {
+
+                            int dg = g-lowPassRadius;
+
+                            int p0 = i+d0+df;
+                            int p1 = j+d1+dg;
+
+                            T val = img.valueOrAlt({p0,p1},0);
+                            accumulated += gaussianCoeffs[f]*gaussianCoeffs[g]*val;
+
+                        }
+                    }
+
+                    filtered[k][l] = accumulated;
+
+                }
+            }
+
+            T forward0 = filtered[2][0] + 2*filtered[2][1] + filtered[2][2];
+            T backward0 = filtered[0][0] + 2*filtered[0][1] + filtered[0][2];
+
+            T forward1 = filtered[0][2] + 2*filtered[1][2] + filtered[2][2];
+            T backward1 = filtered[0][0] + 2*filtered[1][0] + filtered[2][0];
+
+            T d0 = forward0 - backward0;
+            T d1 = forward1 - backward1;
 
             M.atUnchecked(i,j,0) = d0*d0;
             M.atUnchecked(i,j,1) = d0*d1;
@@ -282,6 +374,186 @@ Multidim::Array<T, 2> windowedHarrisCornerScore(Multidim::Array<T, 3, Multidim::
             }
 
             response.atUnchecked(i,j) = score;
+
+        }
+    }
+
+    return response;
+
+}
+
+
+template<typename T>
+Multidim::Array<T, 2> maskedWindowedHarrisCornerScore(Multidim::Array<T, 2, Multidim::ConstView> const& img,
+                                                      Multidim::Array<bool, 2, Multidim::ConstView> const& mask,
+                                                      int windowRadius = 2,
+                                                      int lowPassRadius = 0) {
+
+    Multidim::Array<bool, 2> extendedMask;
+
+    if (windowRadius > 0) {
+        extendedMask = ImageProcessing::dilation(windowRadius, windowRadius, mask);
+    } else {
+        extendedMask = mask;
+    }
+
+    Multidim::Array<T, 3> M = maskedHarrisCornerMMat(img,
+                                                     Multidim::Array<bool, 2, Multidim::ConstView>(extendedMask),
+                                                     lowPassRadius);
+
+    std::array<int,3> shape = M.shape();
+
+    Multidim::Array<T, 2> response(shape[0], shape[1]);
+
+    for (int i = 0; i < shape[0]; i++) {
+        for (int j = 0; j < shape[1]; j++) {
+
+            if (!mask.valueUnchecked(i,j)) {
+                response.atUnchecked(i,j) = 0;
+                continue;
+            }
+
+            T d02 = 0;
+            T d0d1 = 0;
+            T d12 = 0;
+
+            for (int di = -windowRadius; di <= windowRadius; di++) {
+
+                if (i+di < 0 or i+di >= shape[0]) {
+                    continue;
+                }
+
+                for (int dj = -windowRadius; dj <= windowRadius; dj++) {
+
+                    if (j+dj < 0 or j+dj >= shape[1]) {
+                        continue;
+                    }
+
+                    d02 += M.valueUnchecked(i+di,j+dj,0);
+                    d0d1 += M.valueUnchecked(i+di,j+dj,1);
+                    d12 += M.valueUnchecked(i+di,j+dj,2);
+                }
+            }
+
+            T det = d02*d12 - d0d1*d0d1;
+            T tr = d02 + d12;
+
+            T score = det/tr;
+
+            if (std::isinf(score) or std::isnan(score)) {
+                score = 0;
+            }
+
+            response.atUnchecked(i,j) = score;
+
+        }
+    }
+
+    return response;
+}
+
+
+template<typename T>
+Multidim::Array<uint8_t, 2> FASTCornerDetection(Multidim::Array<T, 2, Multidim::ConstView> const& img, T threshold) {
+
+    constexpr int BELOW = -1;
+    constexpr int SAME = 0;
+    constexpr int ABOVE = 1;
+
+    constexpr int maxRadius = 3;
+
+    constexpr int CircleSize = 16;
+
+    std::array<std::array<int,2>,CircleSize> deltas =
+    {std::array<int,2>({-3,0}),
+     std::array<int,2>({-3,1}),
+     std::array<int,2>({-2,2}),
+     std::array<int,2>({-1,3}),
+     std::array<int,2>({0,3}),
+     std::array<int,2>({1,3}),
+     std::array<int,2>({2,2}),
+     std::array<int,2>({3,1}),
+     std::array<int,2>({3,0}),
+     std::array<int,2>({3,-1}),
+     std::array<int,2>({2,-2}),
+     std::array<int,2>({1,-3}),
+     std::array<int,2>({0,-3}),
+     std::array<int,2>({-1,-3}),
+     std::array<int,2>({-2,-2}),
+     std::array<int,2>({-3,-1})};
+
+    auto shape = img.shape();
+    Multidim::Array<uint8_t, 2> response(shape);
+
+    for (int i = 0; i < shape[0]; i++) {
+        for (int j = 0; j < shape[1]; j++) {
+
+            T center = img.valueUnchecked(i,j);
+            std::array<int, CircleSize> pixTypes;
+
+            for (int c = 0; c < CircleSize; c++) {
+
+                int idx = i + deltas[c][0];
+                int jdx = j + deltas[c][1];
+
+                if (idx < 0 or idx >= shape[0] or jdx < 0 or jdx >= shape[1]) {
+                    pixTypes[c] = SAME;
+                } else {
+                    T val = img.valueUnchecked(idx,jdx);
+
+                    if (val < center-threshold) {
+                        pixTypes[c] = BELOW;
+                    } else if (val > center+threshold) {
+                        pixTypes[c] = ABOVE;
+                    } else {
+                        pixTypes[c] = SAME;
+                    }
+                }
+
+            }
+
+            int maxCount = 0;
+            int currentCount = 0;
+            int previousType = SAME;
+
+            for (int c = 0; c < CircleSize; c++) {
+
+                if (pixTypes[c] == SAME) {
+                    previousType = SAME;
+                    currentCount = 0;
+                    continue;
+                }
+
+                if (pixTypes[c] == previousType) {
+                    currentCount++;
+                    if (currentCount > maxCount) {
+                        maxCount = currentCount;
+                    }
+                } else {
+                    previousType = pixTypes[c];
+                    currentCount = 1;
+                }
+
+            }
+
+            //loop around
+            for (int c = 0; c < CircleSize; c++) {
+
+                if (pixTypes[c] == SAME) {
+                    break;
+                }
+
+                if (pixTypes[c] != previousType) {
+                    break;
+                }
+
+                currentCount++;
+                if (currentCount > maxCount) {
+                    maxCount = currentCount;
+                }
+            }
+
+            response.atUnchecked(i,j) = maxCount;
 
         }
     }
--- test/unittests/testSparseMatchingUtils.cpp
+++ test/unittests/testSparseMatchingUtils.cpp
@@ -1,5 +1,6 @@
 #include <QtTest/QtTest>
 
+#include "sparseMatching/cornerDetectors.h"
 #include "sparseMatching/nonLocalMaximumPointSelection.h"
 
 #include <iostream>
@@ -34,12 +35,59 @@ Multidim::Array<float,2> generateSampleProblem(std::default_random_engine & re, 
     return ret;
 }
 
+std::tuple<Multidim::Array<float,2>, std::array<std::array<int,2>,4>> generateSquare(int width) {
+
+    int squareWidth = 2*width;
+
+    int b1 = width/2;
+    int b2 = b1+width;
+
+    std::array<std::array<int,2>,4> coords = {std::array<int,2>({b1,b1}),
+                                              std::array<int,2>({b1,b2}),
+                                              std::array<int,2>({b2,b1}),
+                                              std::array<int,2>({b2,b2})};
+
+    Multidim::Array<float,2> ret(squareWidth, squareWidth);
+
+    for (int i = 0; i < squareWidth; i++) {
+        for (int j = 0; j < squareWidth; j++) {
+
+            bool iInRange = i >= b1 and i <= b2;
+            bool jInRange = j >= b1 and j <= b2;
+
+            bool iOnRange = i == b1 or i == b2;
+            bool jOnRange = j == b1 or j == b2;
+
+            float color = 0;
+
+            if (iOnRange and jInRange) {
+                color = 0.5;
+            } else if (jOnRange and iInRange) {
+                color = 0.5;
+            } else if (iInRange and jInRange) {
+                color = 1;
+            }
+
+            ret.atUnchecked(i,j) = color;
+        }
+    }
+
+    return std::make_tuple(ret, coords);
+
+}
+
 class TestSparseMatchingUtils: public QObject{
 
     Q_OBJECT
 private Q_SLOTS:
 
     void initTestCase();
+
+    void testHarrisCornerDetector();
+
+    void testMaskedHarrisCornerMMat();
+
+    void testFastCornerDetector();
 
     void testNonMaximumPointSelection();
 
@@ -52,6 +100,165 @@ void TestSparseMatchingUtils::initTestCase() {
     srand((unsigned int) time(nullptr));
     std::random_device rd;
     re.seed(rd());
+}
+
+void TestSparseMatchingUtils::testHarrisCornerDetector() {
+
+    constexpr int squareWidth = 30;
+    constexpr int lp_radius = 2;
+    constexpr int nm_radius = 4;
+
+    auto [img, gt_points] = generateSquare(squareWidth);
+
+    Multidim::Array<float, 2> score = HarrisCornerScore(Multidim::Array<float, 2, Multidim::ConstView>(img), lp_radius);
+
+    std::vector<std::array<float, 2>> results = nonLocalMaximumPointSelection(Multidim::Array<float, 2, Multidim::ConstView>(score), nm_radius, 0.f);
+
+    QCOMPARE(results.size(), gt_points.size());
+
+    for (int i = 0; i < results.size(); i++) {
+        float minDist = std::numeric_limits<float>::infinity();
+
+        for (int j = 0; j < gt_points.size(); j++) {
+
+            float d1 = gt_points[j][0] - results[i][0];
+            float d2 = gt_points[j][1] - results[i][1];
+            float dist = sqrt(d1*d1 + d2*d2);
+
+            if (dist < minDist) {
+                minDist = dist;
+            }
+        }
+
+        QVERIFY2(minDist < sqrt(2), qPrintable(QString("Min distance too large for pt %1").arg(i)));
+    }
+
+    constexpr int w_radius = 0;
+
+    Multidim::Array<float, 2> score_w = windowedHarrisCornerScore(Multidim::Array<float, 2, Multidim::ConstView>(img), w_radius, lp_radius);
+
+    results = nonLocalMaximumPointSelection(Multidim::Array<float, 2, Multidim::ConstView>(score_w), nm_radius, 0.f);
+
+    QCOMPARE(results.size(), gt_points.size());
+
+    for (int i = 0; i < results.size(); i++) {
+        float minDist = std::numeric_limits<float>::infinity();
+
+        for (int j = 0; j < gt_points.size(); j++) {
+            float d1 = gt_points[j][0] - results[i][0];
+            float d2 = gt_points[j][1] - results[i][1];
+            float dist = sqrt(d1*d1 + d2*d2);
+
+            if (dist < minDist) {
+                minDist = dist;
+            }
+        }
+
+        QVERIFY2(minDist < sqrt(2), qPrintable(QString("Min distance too large for pt %1").arg(i)));
+    }
+
+}
+
+
+
+void TestSparseMatchingUtils::testMaskedHarrisCornerMMat() {
+
+    constexpr int size = 30;
+    constexpr int lp_radius = 2;
+    constexpr int nm_radius = 4;
+
+    Multidim::Array<float,2> img(size, size);
+    Multidim::Array<bool,2> mask(size, size);
+
+    std::uniform_real_distribution<float> imgDist(-1,1);
+    std::uniform_int_distribution<uint8_t> maskDist(0,1);
+
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+
+            img.atUnchecked(i,j) = imgDist(re);
+            mask.atUnchecked(i,j) = maskDist(re);
+
+        }
+    }
+
+    Multidim::Array<float, 3> base = HarrisCornerMMat(Multidim::Array<float, 2, Multidim::ConstView>(img),
+                                                      lp_radius);
+    Multidim::Array<float, 3> masked = maskedHarrisCornerMMat(Multidim::Array<float, 2, Multidim::ConstView>(img),
+                                                              Multidim::Array<bool, 2, Multidim::ConstView>(mask),
+                                                              lp_radius);
+
+    QCOMPARE(masked.shape(), base.shape());
+
+    for (int i = lp_radius+1; i < size-lp_radius-1; i++) {
+        for (int j = lp_radius+1; j < size-lp_radius-1; j++) {
+
+            if (mask.valueUnchecked(i,j)) {
+
+                for (int c = 0; c < 3; c++) {
+                    float tol = 1e-4;
+                    float delta = std::abs(base.valueUnchecked(i,j, c) - masked.valueUnchecked(i,j, c));
+
+                    QVERIFY2(delta < tol, qPrintable(QString("Different M matrix estimates results, error = %1, i = %2, j = %3.").arg(delta).arg(i).arg(j)));
+                }
+            } else {
+
+                for (int c = 0; c < 3; c++) {
+                    QCOMPARE(masked.valueUnchecked(i,j,c), 0);
+                }
+            }
+
+        }
+    }
+
+}
+
+void TestSparseMatchingUtils::testFastCornerDetector() {
+
+    constexpr int squareWidth = 30;
+    constexpr int thresh = 10;
+    constexpr float fastThres = 0.05;
+    constexpr int lp_radius = 2;
+    constexpr int w_radius = 1; //windows radius need to be 0, else the corner position of windowed harris corner detector can be unpredictable.
+    constexpr int nm_radius = 4;
+
+    auto [img, gt_points] = generateSquare(squareWidth);
+
+    Multidim::Array<uint8_t, 2> fast_score = FASTCornerDetection(Multidim::Array<float, 2, Multidim::ConstView>(img), fastThres);
+
+    auto m_shape = fast_score.shape();
+    Multidim::Array<bool, 2> mask(m_shape);
+
+    for(int i = 0; i < m_shape[0]; i++) {
+        for(int j = 0; j < m_shape[1]; j++) {
+            mask.atUnchecked(i,j) = fast_score.valueUnchecked(i,j) >= thresh;
+        }
+    }
+
+    Multidim::Array<float, 2> score = maskedWindowedHarrisCornerScore(Multidim::Array<float, 2, Multidim::ConstView>(img),
+                                                                      Multidim::Array<bool, 2, Multidim::ConstView>(mask),
+                                                                      w_radius);
+
+    std::vector<std::array<float, 2>> results = nonLocalMaximumPointSelection(Multidim::Array<float, 2, Multidim::ConstView>(score), nm_radius, 0.f);
+
+    QCOMPARE(results.size(), gt_points.size());
+
+    for (int i = 0; i < results.size(); i++) {
+        float minDist = std::numeric_limits<float>::infinity();
+
+        for (int j = 0; j < gt_points.size(); j++) {
+            float d1 = gt_points[j][0] - results[i][0];
+            float d2 = gt_points[j][1] - results[i][1];
+            float dist = sqrt(d1*d1 + d2*d2);
+
+            if (dist < minDist) {
+                minDist = dist;
+            }
+        }
+
+        QVERIFY2(minDist < sqrt(2), qPrintable(QString("Min distance too large for pt %1").arg(i)));
+    }
+
 }
 
 void TestSparseMatchingUtils::testNonMaximumPointSelection() {
