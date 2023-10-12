a5b755d9f773efe766294fc67ae01fad898e9774
Laurent Valentin Jospin <laurent.jospin@epfl.ch>
2023-10-12 12:27:29 +0200

    Fix some templated functions of the geometry package
    Type handling has been corrected such that these functions are
    compatible with the Jet type in Ceres.

--- geometry/rotations.h
+++ geometry/rotations.h
@@ -3,7 +3,7 @@
 
 /*LibStevi, or the Stereo Vision Library, is a collection of utilities for 3D computer vision.
 
-Copyright (C) 2021-2022 Paragon<french.paragon@gmail.com>
+Copyright (C) 2021-2023 Paragon<french.paragon@gmail.com>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -27,174 +27,174 @@ namespace Geometry {
 
 template<typename T>
 Eigen::Matrix<T,3,3> rodriguezFormula(Eigen::Matrix<T,3,1> const& r) {
-	T theta = r.norm();
-	Eigen::Matrix<T,3,3> m = skew(r);
+    T theta = r.norm();
+    Eigen::Matrix<T,3,3> m = skew(r);
 
-	Eigen::Matrix<T,3,3> R;
+    Eigen::Matrix<T,3,3> R;
 
-	if (theta > 1e-6) {
-		R = Eigen::Matrix<T,3,3>::Identity() + sin(theta)/theta*m + (1 - cos(theta))/(theta*theta)*m*m;
-	} else {
-		R = Eigen::Matrix<T,3,3>::Identity() + m + 0.5*m*m;
-	}
+    if (theta > 1e-6) {
+        R = Eigen::Matrix<T,3,3>::Identity() + sin(theta)/theta*m + (T(1) - cos(theta))/(theta*theta)*m*m;
+    } else {
+        R = Eigen::Matrix<T,3,3>::Identity() + m + 0.5*m*m;
+    }
 
-	return R;
+    return R;
 }
 
 template<typename T>
 Eigen::Matrix<T,3,1> inverseRodriguezFormula(Eigen::Matrix<T,3,3> const& R) {
 
-	T d =  0.5*(R(0,0) + R(1,1) + R(2,2) - 1);
-	Eigen::Matrix<T,3,1> omega;
+    T d =  0.5*(R(0,0) + R(1,1) + R(2,2) - 1);
+    Eigen::Matrix<T,3,1> omega;
 
-	Eigen::Matrix<T,3,1> dR = unskew<T>(R - R.transpose());
+    Eigen::Matrix<T,3,1> dR = unskew<T>(R - R.transpose());
 
-	T nDr = dR.norm();
+    T nDr = dR.norm();
 
-	if (d>0.999)
-	{
-	  omega=0.5*dR;
-	}
-	else if (nDr < 1e-3) {
-		T theta = acos(d);
-		Eigen::Matrix<T,3,1> d = R.diagonal();
-		omega = theta*(d - Eigen::Matrix<T,3,1>::Ones()*d.minCoeff())/(1 - d.minCoeff());
-	}
-	else
-	{
-	  float theta = acos(d);
-	  omega = theta/(2*sqrt(1-d*d))*dR;
-	}
+    if (d>0.999)
+    {
+      omega=0.5*dR;
+    }
+    else if (nDr < 1e-3) {
+        T theta = acos(d);
+        Eigen::Matrix<T,3,1> d = R.diagonal();
+        omega = theta*(d - Eigen::Matrix<T,3,1>::Ones()*d.minCoeff())/(1 - d.minCoeff());
+    }
+    else
+    {
+      float theta = acos(d);
+      omega = theta/(2*sqrt(1-d*d))*dR;
+    }
 
-	return omega;
+    return omega;
 }
 
 template<typename T>
 Eigen::Matrix<T,3,3> diffRodriguezLieAlgebra(Eigen::Matrix<T,3,1> const& r) {
 
-	float theta = r.norm();
-	Eigen::Matrix<T,3,3> m = skew(r);
+    float theta = r.norm();
+    Eigen::Matrix<T,3,3> m = skew(r);
 
-	Eigen::Matrix<T,3,3> dR;
+    Eigen::Matrix<T,3,3> dR;
 
-	float a;
-	float b;
-	float c;
+    float a;
+    float b;
+    float c;
 
-	if (theta > 1e-6) {
-		a = sin(theta)/theta;
-		b = (1 - cos(theta))/(theta*theta);
-		c = (1 - a)/(theta*theta);
-	} else {
-		a = 1;
-		b = 1./2.;
-		c = 1./6.;
-	}
+    if (theta > 1e-6) {
+        a = sin(theta)/theta;
+        b = (1 - cos(theta))/(theta*theta);
+        c = (1 - a)/(theta*theta);
+    } else {
+        a = 1;
+        b = 1./2.;
+        c = 1./6.;
+    }
 
-	dR = a*Eigen::Matrix<T,3,3>::Identity() + b*m + c*(r*r.transpose());
+    dR = a*Eigen::Matrix<T,3,3>::Identity() + b*m + c*(r*r.transpose());
 
-	return dR;
+    return dR;
 }
 
 template<typename T>
 Eigen::Matrix<T,3,3> diffRodriguez(Eigen::Matrix<T,3,1> const& r, Axis direction) {
 
-	float theta = r.norm();
+    float theta = r.norm();
 
-	int i = (direction == Axis::X) ? 0 : ((direction == Axis::Y) ? 1 : 2);
+    int i = (direction == Axis::X) ? 0 : ((direction == Axis::Y) ? 1 : 2);
 
-	Eigen::Matrix<T,3,1> eye = Eigen::Matrix<T,3,1>::Zero();
-	eye[i] = 1;
+    Eigen::Matrix<T,3,1> eye = Eigen::Matrix<T,3,1>::Zero();
+    eye[i] = 1;
 
-	if (theta < 1e-6) {
-		return skew(eye);
-	}
+    if (theta < 1e-6) {
+        return skew(eye);
+    }
 
-	Eigen::Matrix<T,3,3> cross = skew(r);
-	Eigen::Matrix<T,3,3> R = rodriguezFormula(r);
-	return (r[i]*cross + skew<T>(cross*(eye - R.col(i))))/(theta*theta) * R;
+    Eigen::Matrix<T,3,3> cross = skew(r);
+    Eigen::Matrix<T,3,3> R = rodriguezFormula(r);
+    return (r[i]*cross + skew<T>(cross*(eye - R.col(i))))/(theta*theta) * R;
 }
 
 template<typename T>
 class ShapePreservingTransform
 {
 public:
-	EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 
-	ShapePreservingTransform(Eigen::Matrix<T,3,1> r, Eigen::Matrix<T,3,1> t, T s) :
-		t(t), r(r), s(s)
-	{
+    ShapePreservingTransform(Eigen::Matrix<T,3,1> r, Eigen::Matrix<T,3,1> t, T s) :
+        t(t), r(r), s(s)
+    {
 
-	}
-	ShapePreservingTransform() :
-		t(Eigen::Matrix<T,3,1>::Zero()), r(Eigen::Matrix<T,3,1>::Zero()), s(1.)
-	{
+    }
+    ShapePreservingTransform() :
+        t(Eigen::Matrix<T,3,1>::Zero()), r(Eigen::Matrix<T,3,1>::Zero()), s(1.)
+    {
 
-	}
+    }
 
-	Eigen::Matrix<T,3,1> operator*(Eigen::Matrix<T,3,1> const& pt) const {
-		return s*rodriguezFormula(r)*pt + t;
-	}
-	Eigen::Matrix<T,3,Eigen::Dynamic> operator*(Eigen::Matrix<T,3,Eigen::Dynamic> const& pts) const {
-		return applyOnto(pts.array()).matrix();
-	}
-	Eigen::Array<T,3,Eigen::Dynamic> operator*(Eigen::Array<T,3,Eigen::Dynamic> const& pts) const {
-		return applyOnto(pts);
-	}
+    Eigen::Matrix<T,3,1> operator*(Eigen::Matrix<T,3,1> const& pt) const {
+        return s*rodriguezFormula(r)*pt + t;
+    }
+    Eigen::Matrix<T,3,Eigen::Dynamic> operator*(Eigen::Matrix<T,3,Eigen::Dynamic> const& pts) const {
+        return applyOnto(pts.array()).matrix();
+    }
+    Eigen::Array<T,3,Eigen::Dynamic> operator*(Eigen::Array<T,3,Eigen::Dynamic> const& pts) const {
+        return applyOnto(pts);
+    }
 
-	ShapePreservingTransform<T> operator*(ShapePreservingTransform<T> const& other) const {
-		Eigen::Matrix<T,3,3> R = rodriguezFormula(r);
-		Eigen::Matrix<T,3,3> Rc = R*rodriguezFormula(other.r);
-		return ShapePreservingTransform(inverseRodriguezFormula(Rc), s*R*other.t + t, s*other.s);
-	}
+    ShapePreservingTransform<T> operator*(ShapePreservingTransform<T> const& other) const {
+        Eigen::Matrix<T,3,3> R = rodriguezFormula(r);
+        Eigen::Matrix<T,3,3> Rc = R*rodriguezFormula(other.r);
+        return ShapePreservingTransform(inverseRodriguezFormula(Rc), s*R*other.t + t, s*other.s);
+    }
 
-	AffineTransform<T> toAffineTransform() const {
-		return AffineTransform<T>(s*rodriguezFormula(r), t);
-	}
-	ShapePreservingTransform<T> inverse() const {
-		return ShapePreservingTransform<T>(-r, - rodriguezFormula(r).transpose()*t/s, 1/s);
-	}
+    AffineTransform<T> toAffineTransform() const {
+        return AffineTransform<T>(s*rodriguezFormula(r), t);
+    }
+    ShapePreservingTransform<T> inverse() const {
+        return ShapePreservingTransform<T>(-r, - rodriguezFormula(r).transpose()*t/s, 1/s);
+    }
 
-	inline bool isFinite() const {
-		return t.array().isFinite().all() and r.array().isFinite().all() and std::isfinite(s);
-	}
+    inline bool isFinite() const {
+        return t.array().isFinite().all() and r.array().isFinite().all() and std::isfinite(s);
+    }
 
-	Eigen::Matrix<T,3,1> t;
-	Eigen::Matrix<T,3,1> r;
-	T s;
+    Eigen::Matrix<T,3,1> t;
+    Eigen::Matrix<T,3,1> r;
+    T s;
 
 protected:
 
-	Eigen::Array<T,3,Eigen::Dynamic> applyOnto(Eigen::Array<T,3,Eigen::Dynamic> const& pts) const {
-		Eigen::Array3Xf transformedPts;
-		transformedPts.resize(3, pts.cols());
+    Eigen::Array<T,3,Eigen::Dynamic> applyOnto(Eigen::Array<T,3,Eigen::Dynamic> const& pts) const {
+        Eigen::Array3Xf transformedPts;
+        transformedPts.resize(3, pts.cols());
 
-		for (int i = 0; i < transformedPts.cols(); i++) {
-			transformedPts.col(i) = s*rodriguezFormula(r)*(pts.col(i).matrix()) + t;
-		}
+        for (int i = 0; i < transformedPts.cols(); i++) {
+            transformedPts.col(i) = s*rodriguezFormula(r)*(pts.col(i).matrix()) + t;
+        }
 
-		return transformedPts;
-	}
+        return transformedPts;
+    }
 };
 
 
 
 template <typename Scalar>
 Eigen::Matrix<Scalar, 3, 3> eulerRadXYZToRotation(Scalar eulerX,
-												  Scalar eulerY,
-												  Scalar eulerZ) {
+                                                  Scalar eulerY,
+                                                  Scalar eulerZ) {
 
-	return (Eigen::AngleAxis<Scalar>(eulerX, Eigen::Matrix<Scalar,3,1>::UnitX())*
-			Eigen::AngleAxis<Scalar>(eulerY, Eigen::Matrix<Scalar,3,1>::UnitY())*
-			Eigen::AngleAxis<Scalar>(eulerZ, Eigen::Matrix<Scalar,3,1>::UnitZ())).toRotationMatrix();
+    return (Eigen::AngleAxis<Scalar>(eulerX, Eigen::Matrix<Scalar,3,1>::UnitX())*
+            Eigen::AngleAxis<Scalar>(eulerY, Eigen::Matrix<Scalar,3,1>::UnitY())*
+            Eigen::AngleAxis<Scalar>(eulerZ, Eigen::Matrix<Scalar,3,1>::UnitZ())).toRotationMatrix();
 
 }
 
 template <typename Scalar>
 Eigen::Matrix<Scalar, 3, 3> eulerDegXYZToRotation(Scalar eulerX,
-												  Scalar eulerY,
-												  Scalar eulerZ) {
-	return eulerRadXYZToRotation(Scalar(eulerX/180*M_PI), Scalar(eulerY/180*M_PI), Scalar(eulerZ/180*M_PI));
+                                                  Scalar eulerY,
+                                                  Scalar eulerZ) {
+    return eulerRadXYZToRotation(Scalar(eulerX/180*M_PI), Scalar(eulerY/180*M_PI), Scalar(eulerZ/180*M_PI));
 }
 
 } // namespace Geometry
