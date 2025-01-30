0dcccfcf96e0fdb3d6ca5a94b130c2ce3314d5ec
R-Maggio <raphael.maggio.aprile@gmail.com>
2025-01-30 10:35:26 +0100

    adapter to remove attributes or hide the color

--- examples/io_remove_attributes/main.cpp
+++ examples/io_remove_attributes/main.cpp
@@ -3,7 +3,7 @@
 #include <filesystem>
 #include <algorithm>
 #include <vector>
-#include "io/attributeMapper.h"
+#include "io/attributeRemover.h"
 
 // function that converts any std::vector to a string
 template <typename T>
@@ -17,20 +17,20 @@ std::ostream &operator<<(std::ostream &os, const std::vector<T> &vec) {
 int main(int argc, char const *argv[]) {
 
     std::filesystem::path lasFilePath;
+    std::vector<std::string> attributesToRemove;
 
     if (argc > 1) {
         lasFilePath = argv[1];
-    }
-	else {
+    } else {
 		std::cout << "No las file provided" << std::endl;
 		return 1;
 	}
 
-    std::cout << "This will open a file and replace map the attributes original x to y, original y to x and original z to newZ" << std::endl;
-    std::map<std::string, std::string> attributeMap = {{"x", "y"}, {"y", "x"}, {"z", "newZ"}};
-    bool onlyKeepAttributesInMap = false;
-	// start timer
-	auto startTime = std::chrono::high_resolution_clock::now();
+    if (argc > 2) {
+        attributesToRemove = std::vector<std::string>(argv + 2, argv + argc);
+    }
+
+    std::cout << "This will open a pointcloud and remove attributes" << std::endl;
 
     auto fullAccessOpt = StereoVision::IO::openPointCloud(lasFilePath);
 
@@ -39,6 +39,13 @@ int main(int argc, char const *argv[]) {
         return 1;
     }
     std::cout << "file opened" << std::endl;
+
+    // attributes that will be removed
+    std::cout<< "Attributes to remove: ";
+    for (auto& att : attributesToRemove) {
+        std::cout << att << ' ';
+    }
+    std::cout << std::endl;
 
     auto& fullAccess = *fullAccessOpt;
     auto& header = fullAccess.headerAccess;
@@ -60,7 +67,7 @@ int main(int argc, char const *argv[]) {
 
     std::cout << "\n\n";
 
-    std::cout << "--------------- first point before map ---------------\n";
+    std::cout << "--------------- first point before remove ---------------\n";
     for (auto& att : cloudpoint->attributeList()) {
         std::cout << att << ": " << StereoVision::IO::castedPointCloudAttribute<std::string>(cloudpoint->getAttributeByName(att.c_str()).value_or(std::string{})) << '\n';
     }
@@ -73,7 +80,7 @@ int main(int argc, char const *argv[]) {
         std::cout << "point color: " << pointColor->r << ' ' << pointColor->g << ' ' << pointColor->b << ' ' << pointColor->a << '\n';
     }
 
-    std::cout << "--------------first point after map -----------------\n";
+    std::cout << "--------------first point after remove -----------------\n";
     
     // re-open the file
     auto fullAccessOpt2 = StereoVision::IO::openPointCloud(lasFilePath);
@@ -83,10 +90,11 @@ int main(int argc, char const *argv[]) {
     }
     std::cout << "file opened" << std::endl;
     auto& fullAccess2 = *fullAccessOpt2;
+    auto fullAccessRemover = std::make_unique<StereoVision::IO::FullPointCloudAccessInterface>(std::move(fullAccess2));
 
-    auto fullAccessMap = std::move(mapPointCloudAttributes(std::move(*fullAccessOpt2), attributeMap, onlyKeepAttributesInMap));
-    
-    auto& cloudpoint2 = fullAccessMap.pointAccess;
+    fullAccessRemover = std::move(StereoVision::IO::RemoveAttributesOrColorFromPointCloud(fullAccessRemover, attributesToRemove));
+
+    auto& cloudpoint2 = fullAccessRemover->pointAccess;
 
     for (auto& att : cloudpoint2->attributeList()) {
         std::cout << att << ": " << StereoVision::IO::castedPointCloudAttribute<std::string>(cloudpoint2->getAttributeByName(att.c_str()).value_or(std::string{})) << '\n';
@@ -95,6 +103,36 @@ int main(int argc, char const *argv[]) {
     pointGeo = cloudpoint2->castedPointGeometry<double>();
     std::cout << "point geometry: " << pointGeo.x << ' ' << pointGeo.y << ' ' << pointGeo.z << '\n';
     pointColor = cloudpoint2->castedPointColor<double>();
+    if (pointColor.has_value()) {
+        std::cout << "point color: " << pointColor->r << ' ' << pointColor->g << ' ' << pointColor->b << ' ' << pointColor->a << '\n';
+    }
+    
+    std::cout << "--------------first point after removing all attributes -----------------\n";
+
+    fullAccessRemover = std::move(StereoVision::IO::RemoveAttributesOrColorFromPointCloud(fullAccessRemover, {}, std::nullopt, true));
+
+    for (auto& att : fullAccessRemover->pointAccess->attributeList()) {
+        std::cout << att << ": " << StereoVision::IO::castedPointCloudAttribute<std::string>(fullAccessRemover->pointAccess->getAttributeByName(att.c_str()).value_or(std::string{})) << '\n';
+    }
+    // point
+    pointGeo = fullAccessRemover->pointAccess->castedPointGeometry<double>();
+    std::cout << "point geometry: " << pointGeo.x << ' ' << pointGeo.y << ' ' << pointGeo.z << '\n';
+    pointColor = fullAccessRemover->pointAccess->castedPointColor<double>();
+    if (pointColor.has_value()) {
+        std::cout << "point color: " << pointColor->r << ' ' << pointColor->g << ' ' << pointColor->b << ' ' << pointColor->a << '\n';
+    }
+
+    std::cout << "--------------first point after removing color -----------------\n";
+
+    fullAccessRemover = std::move(StereoVision::IO::RemoveAttributesOrColorFromPointCloud(fullAccessRemover, {}, true, std::nullopt));
+
+    for (auto& att : fullAccessRemover->pointAccess->attributeList()) {
+        std::cout << att << ": " << StereoVision::IO::castedPointCloudAttribute<std::string>(fullAccessRemover->pointAccess->getAttributeByName(att.c_str()).value_or(std::string{})) << '\n';
+    }
+    // point
+    pointGeo = fullAccessRemover->pointAccess->castedPointGeometry<double>();
+    std::cout << "point geometry: " << pointGeo.x << ' ' << pointGeo.y << ' ' << pointGeo.z << '\n';
+    pointColor = fullAccessRemover->pointAccess->castedPointColor<double>();
     if (pointColor.has_value()) {
         std::cout << "point color: " << pointColor->r << ' ' << pointColor->g << ' ' << pointColor->b << ' ' << pointColor->a << '\n';
     }
